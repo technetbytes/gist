ab9758f27e92671b9310a05cf4e023e01787e704
technetbytes <saqibullah@gmail.com>
2022-02-14 16:12:30 +0500

    add scheduler in client application for messaging

--- client/client_app.py
+++ client/client_app.py
@@ -1,4 +1,17 @@
+import time
+import schedule
 from email_message import json_email_request, simple_email_request
 
-if __name__ == '__main__':
+def messaging():
+    print("sending email ...")
     json_email_request()
+  
+schedule.every(10).seconds.do(messaging)
+
+def start_scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    start_scheduler()
